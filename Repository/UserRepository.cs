2d1f8125d8df5b0a28823955f20b0f8b6b23b406
Hyzmyn <hung2k3olaf@gmail.com>
2024-05-25 22:23:58 +0700

    Initial commit

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Repository.Entities;
+using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UserRepository
+    {
+        private DiamondShopContext _db;
+
+        public List<User> GetAll()
+        {
+            _db = new ();
+            return _db.Users.ToList();
+        }
+    }
+}
