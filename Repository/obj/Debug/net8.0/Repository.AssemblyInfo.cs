66c7e3fed2564833ac2423f86b9ae99dd699c156
Hyzmyn <hung2k3olaf@gmail.com>
2024-05-29 22:24:02 +0700

    test

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b5223da9a17235e9f0dac7efb53c654a497546")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f519572e96c50ceda2564c75d89d24e84cc6bb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
