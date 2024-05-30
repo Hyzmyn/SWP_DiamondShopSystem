554e42a56cd68f40ac95961c9169ddf627638bb6
John Doe <hodinhbinh2503@gmail.com>
2024-05-30 16:08:00 +0700

    dis and material ser

--- DiamondShopSystem/obj/Debug/net8.0/DiamondShopSystem.AssemblyInfo.cs
+++ DiamondShopSystem/obj/Debug/net8.0/DiamondShopSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondShopSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841f3550ce03c0681cd73795f5daeb409be860ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048f384d075355e341d243d23e9a93c8c7d09e41")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondShopSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondShopSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbbc5615a561e84e786b9a795d531158e7ee9f89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048f384d075355e341d243d23e9a93c8c7d09e41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
