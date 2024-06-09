4540b88d5158f3e896dbc4e1d3755d77d41cd8c8
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2024-06-10 00:06:13 +0700

    push worked login, signup

--- DiamondShopSystem/Pages/Index.cshtml.cs
+++ DiamondShopSystem/Pages/Index.cshtml.cs
@@ -3,17 +3,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Repository.Entities;
 using Service.Interface;
 using Service.Service;
+using System.ComponentModel.DataAnnotations;
 
 namespace DiamondShopSystem.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        [Required]
+        public string UserName { get; set; }
 
         [BindProperty]
-        public string Username { get; set; }
-        [BindProperty]
+        [Required]
         public string Password { get; set; }
 
+        [BindProperty]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Message { get; set; }
 
         private readonly IUserService _userService;
         private readonly IJwtService _jwtService;
@@ -24,25 +33,58 @@ namespace DiamondShopSystem.Pages
             _jwtService = jwtService;
         }
 
-        public void OnPost()
+        public void OnPostLogin()
         {
             try
             {
-                User user = _userService.Login(Username, Password);
+                if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(Password))
+                {
+                    Message = "Please fill in all fields.";
+                    throw new Exception(Message);
+                }
+
+                var user = _userService.Login(UserName, Password);
                 if (user != null)
                 {
-                    Console.WriteLine(Username + ":" + Password);
+                    Message = "User Logged In";
                     string token = _jwtService.GenerateToken(user.UserId, (int)user.RoleId);
-                    Console.WriteLine(token);
+                    Console.WriteLine(Message);
                 }
             }
-
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Message = ex.Message;
+                Console.WriteLine(Message);
             }
         }
 
+        public void OnPostRegister()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(Password) || string.IsNullOrEmpty(Email))
+                {
+                    Message = "Please fill in all fields.";
+                    throw new Exception(Message);
+                }
+
+                var user = new User
+                {
+                    UserName = UserName,
+                    Password = Password,
+                    Email = Email
+                };
+
+                _userService.AddUser(user);
+                Message = "User Added";
+                Console.WriteLine(Message);
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+                Console.WriteLine(Message);
+            }
+        }
 
     }
 
