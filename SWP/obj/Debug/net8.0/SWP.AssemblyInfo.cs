538407b567304de7525869f69d020193431977bd
Hyzmyn <hung2k3olaf@gmail.com>
2024-06-18 22:18:29 +0700

    new code FE homepage anh asp customer

--- SWP/obj/Debug/net8.0/SWP.AssemblyInfo.cs
+++ SWP/obj/Debug/net8.0/SWP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3cc6f79305b13e8207ea83cc818140f42879b95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fea76bb444557e69eb5cafc987e46565be53ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
