3964ef3ecde1c4ed3853bdd47b169965affc42ac
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2024-07-03 11:54:30 +0700

    merge

--- SWP/obj/Debug/net8.0/SWP.AssemblyInfo.cs
+++ SWP/obj/Debug/net8.0/SWP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbc543a7f5deed617959a7c146630995fb00590")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+864150e35a22faa39ba62ee742aa8d9f8a23bacc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
