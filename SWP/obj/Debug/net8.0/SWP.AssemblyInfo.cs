1ab9c2ee7263274f1e0c0a49fe8f3e993c3ce768
Hyzmyn <hung2k3olaf@gmail.com>
2024-07-09 18:19:35 +0700

    pull master

--- SWP/obj/Debug/net8.0/SWP.AssemblyInfo.cs
+++ SWP/obj/Debug/net8.0/SWP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90082e740ca20d1c5c421c219034ffa44e65123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1e52367da74a0784acea3e8c78bebbe1fcaf31")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
