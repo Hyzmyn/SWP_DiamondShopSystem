0520c13de119cd2cdbc3cb0aa5e4e0a6ab02416a
John Doe <hodinhbinh2503@gmail.com>
2024-07-11 03:16:17 +0700

    vali of product

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce4aa197c104670be0de9ebc98e2308eaf7dc9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49346eac1d10900ae25cbb446f62f8bbb4a5beea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
